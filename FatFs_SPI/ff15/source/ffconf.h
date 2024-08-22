0a3531fdccec9b802fb6b48c4f16d8ee2b63979c
Matt Evans <matt@ozlabs.org>
2024-08-23 00:43:05 +0100

    Add build option to disable RTC
    When used as a CMake submodule, setting FF_DISABLE_RTC causes
    rtc.c to be omitted and for the FatFS FF_FS_NORTC variable to
    be set.
    
    This is particularly useful on RP2350 builds, which don't have
    an RTC.

--- FatFs_SPI/ff15/source/ffconf.h
+++ FatFs_SPI/ff15/source/ffconf.h
@@ -236,8 +236,9 @@
 /  To enable exFAT, also LFN needs to be enabled. (FF_USE_LFN >= 1)
 /  Note that enabling exFAT discards ANSI C (C89) compatibility. */
 
-
+#ifndef FF_FS_NORTC
 #define FF_FS_NORTC		0
+#endif
 #define FF_NORTC_MON	1
 #define FF_NORTC_MDAY	1
 #define FF_NORTC_YEAR	2022
